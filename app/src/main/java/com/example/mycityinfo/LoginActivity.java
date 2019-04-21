f3b6eb6d228116b0cea8855f02593ecbef692d9d
Uddip Ghosh <uddipghosh19@gmail.com>
2019-04-21 17:49:40 +0530

    Signup and locations of Agartala

--- app/src/main/java/com/example/mycityinfo/LoginActivity.java
+++ app/src/main/java/com/example/mycityinfo/LoginActivity.java
@@ -1,9 +1,11 @@
 package com.example.mycityinfo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -21,6 +23,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
     private Button loginbtn;
     private FirebaseAuth firebaseAuth;
     private Button signup;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,14 +37,29 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
         signup=findViewById(R.id.button);
         loginbtn.setOnClickListener(this);
         signup.setOnClickListener(this);
+        progressDialog = new ProgressDialog(this,R.style.myalertdialog);
 
     }
     public void userlogin(){
+        progressDialog.setTitle("Logging in");
+        progressDialog.setMessage("Please Wait...");
+        progressDialog.show();
         String eml=etemail.getText().toString().trim();
         String psw=etpsw.getText().toString().trim();
+        if (TextUtils.isEmpty(eml))
+        {
+            Toast.makeText(this,"Please enter your email",Toast.LENGTH_SHORT);
+            return;
+        }
+        if (TextUtils.isEmpty(psw))
+        {
+            Toast.makeText(this,"Please enter your password",Toast.LENGTH_SHORT);
+            return;
+        }
         firebaseAuth.signInWithEmailAndPassword(eml,psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
+                progressDialog.dismiss();
                 if(task.isSuccessful()){
                     Intent i = new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(i);
--- app/src/main/java/com/example/mycityinfo/SignupActivity.java
+++ app/src/main/java/com/example/mycityinfo/SignupActivity.java
@@ -1,9 +1,12 @@
 package com.example.mycityinfo;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.widget.CircularProgressDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,54 +17,95 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 
-public class SignupActivity extends AppCompatActivity implements View.OnClickListener{
+public class SignupActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private EditText etemail;
-    private EditText etpsw;
-    private EditText etcpsw;
-    private Button loginbtn;
-    private FirebaseAuth firebaseAuth;
+    private EditText textemail;
+    private EditText textpassword;
+    private EditText textcnfpassword;
     private Button signup;
+    private Button login;
+    private FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
+        setContentView(R.layout.signup);
 
-        etemail=findViewById(R.id.editText);
-        etpsw=findViewById(R.id.editText3);
-        loginbtn=findViewById(R.id.button2);
-        firebaseAuth=FirebaseAuth.getInstance();
-        signup=findViewById(R.id.button4);
-        loginbtn.setOnClickListener(this);
+        textemail = findViewById(R.id.editText);
+        textpassword = findViewById(R.id.editText3);
+        textcnfpassword = findViewById(R.id.editText5);
+        signup = findViewById(R.id.button4);
+        login = findViewById(R.id.button2);
+        firebaseAuth = FirebaseAuth.getInstance();
+        progressDialog = new ProgressDialog(this,R.style.myalertdialog);
+
         signup.setOnClickListener(this);
-
-    }
-    public void userlogin(){
-        String eml=etemail.getText().toString().trim();
-        String psw=etcpsw.getText().toString().trim();
-        String cpsw =etcpsw.getText().toString().trim();
-        firebaseAuth.signInWithEmailAndPassword(eml,psw).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if(task.isSuccessful()){
-                    Intent i = new Intent(SignupActivity.this,MainActivity.class);
-                    Toast.makeText(SignupActivity.this, "Account Created", Toast.LENGTH_SHORT).show();
-                    startActivity(i);
-                }
+        login.setOnClickListener(this);
 
 
-            }
-        });
     }
 
     @Override
     public void onClick(View v) {
-
-        if(v == loginbtn){
-            Intent i=new Intent(this,LoginActivity.class);
+        if (v == login)
+        {
+            Intent i = new Intent(this,LoginActivity.class);
             startActivity(i);
+        }
+        if(v == signup)
+        {
+            signitup();
+        }
+
+    }
+    public void signitup()
+    {
+        String email = textemail.getText().toString().trim();
+        String password = textpassword.getText().toString();
+        String cnfpassword = textcnfpassword.getText().toString();
+
+        if(TextUtils.isEmpty(email))
+        {
+            Toast.makeText(this,"Please enter email",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(password))
+        {
+            Toast.makeText(this,"Please enter a password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(TextUtils.isEmpty(cnfpassword))
+        {
+            Toast.makeText(this,"Please re-enter password",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        if(!TextUtils.equals(password,cnfpassword))
+        {
+            Toast.makeText(this,"Passwords does not match",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        else
+        {
+            progressDialog.setTitle("Signing Up");
+            progressDialog.setMessage("Please Wait...");
+            progressDialog.show();
+            firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    progressDialog.dismiss();
+                    if(task.isSuccessful())
+                    {
+                        Toast.makeText(SignupActivity.this, "Signed up successfully", Toast.LENGTH_SHORT).show();
+                        Intent i = new Intent(SignupActivity.this, LoginActivity.class);
+                        startActivity(i);
+                    }
+                    else
+                    {
+                        Toast.makeText(SignupActivity.this,"Failed to sign up",Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
         }
     }
 }
